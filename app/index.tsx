90e49dba0967d9926810ce21a2a0360f0f76d64a
ike_iloegbu <ike@i2mastering.com>
2025-05-06 08:18:03 -0400

    Commit after fixed error. Cleared out cache files and re-added them. This was done in terminal.

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,29 @@
-import { Text, View } from "react-native";
+// add text fields for user input of latitude and longitude coordinates
+// implement weather forecast
+//
+// display results
+
+
+import React, {useState } from "react";
+import { Text, View, TextInput, Button, StyleSheet, ScrollView } from "react-native";
 
 export default function Index() {
+    const [latitude, setLatitude] = useState("");
+    const [longitude, setLongitude] = useState("");
+    const [forecast, setForecast] = useState("")
+    
+    const fetchForecast = async() => {
+        const lat = parseFloat(latitude);
+        const lon = parseFloat(longitude);
+        
+        if(isNaN(lat)) || isNaN(lon){
+            setForecast("Invalid Latitude or longitude");
+            return;
+        }
+        try {
+            const pointRes = await
+        }
+    }
   return (
     <View
       style={{
@@ -9,7 +32,20 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <TextInput
+              style={styles.input}
+              placeholder="Latitude"
+              keyboardType="decimal-pad"
+              value={latitude}
+              onChangeText={setLatitude}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Longitude"
+              keyboardType="decimal-pad"
+              value={longitude}
+              onChangeText={setLongitude}
+            />
     </View>
   );
 }
